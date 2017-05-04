99aa408790b063c80dbfbbaa977e52daf5a7a371
Omar Abou Dehn <3omar.a.de@gmail.com>
2017-05-04 11:10:38 +0200

    Edited

--- FirstPart/readFile.js
+++ FirstPart/readFile.js
@@ -12,8 +12,8 @@ fs.readFile('countries.json', 'utf-8', function(err, data) {
 function print(country, countriesInfo){
 	for (var i = 0 ; i < countriesInfo.length ; i++){
 		if(countriesInfo[i].name === country){
-			console.log("Country:" + " " + countriesInfo[i].name)
-			console.log("Top Level Domain:" + " " + countriesInfo[i].topLevelDomain[0])
+			console.log("Country: " + countriesInfo[i].name)
+			console.log("Top Level Domain: " + countriesInfo[i].topLevelDomain[0])
 		}
 	}
 }
--- SecondPart/callback.js
+++ SecondPart/callback.js
@@ -3,8 +3,8 @@ var jsonFileReader = require("./jsonFileReader")
 function print(countriesInfo){
 	for (var i = 0 ; i < countriesInfo.length ; i++){
 		if(countriesInfo[i].name === process.argv[2]){
-			console.log("Country:" + " " + countriesInfo[i].name)
-			console.log("Top Level Domain:" + " " + countriesInfo[i].topLevelDomain[0])
+			console.log("Country: " + countriesInfo[i].name)
+			console.log("Top Level Domain: " + countriesInfo[i].topLevelDomain[0])
 		}
 	}
 }
